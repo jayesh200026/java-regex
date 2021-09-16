1495c22e3fd4414f017609016292d01a500dc58e
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-16 14:50:54 +0530

    Added feature to accept a zip which has exactly one space after 3 digit

--- zipcode/PinCode.java
+++ zipcode/PinCode.java
@@ -8,14 +8,15 @@ import java.util.regex.Pattern;
 public class PinCode {
 	public static void main(String args[]) {
 
-		String pin = "400088B";
-		String pattern = "^[1-9][0-9]{5}$"; // first digit should be 1-9 followed by 5 digits between 0-9
+		String pin1 = "400 088";
+		String pin2 = "400  088"; //two spaces should be invalid
+		String pattern = "^[1-9][0-9]{2}\\s?[0-9]{3}$"; // first digit should be 1-9 followed by 5 digits between 0-9
 											// Usecase 1 suits the usecase2 as we made it clear that it should start
 											// with digit.
 											
 
-		System.out.println(Pattern.matches(pattern, pin));
-
+		System.out.println(Pattern.matches(pattern, pin1));
+		System.out.println(Pattern.matches(pattern, pin2));
 	}
 
 }
