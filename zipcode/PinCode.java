9ba31d6b64a81b60b4d17846031b2f7e74fce899
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-16 14:42:50 +0530

    Added feature to check if pincode ends with a valid digit or not

--- zipcode/PinCode.java
+++ zipcode/PinCode.java
@@ -8,9 +8,11 @@ import java.util.regex.Pattern;
 public class PinCode {
 	public static void main(String args[]) {
 
-		String pin = "A400088";
-		String pattern = "^[1-9][0-9]{5}"; // first digit should be 1-9 followed by 5 digits between 0-9
-										  // Usecase 1 suits the usecase2 as we made it clear that it should start with digit.
+		String pin = "400088B";
+		String pattern = "^[1-9][0-9]{5}$"; // first digit should be 1-9 followed by 5 digits between 0-9
+											// Usecase 1 suits the usecase2 as we made it clear that it should start
+											// with digit.
+											
 
 		System.out.println(Pattern.matches(pattern, pin));
 
