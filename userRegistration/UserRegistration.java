d44eb77894355fb37a8e931ae1332a94fc259414
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-16 16:05:41 +0530

    Added feature to check valid email id

--- userRegistration/UserRegistration.java
+++ userRegistration/UserRegistration.java
@@ -10,8 +10,14 @@ public class UserRegistration {
 		final String FIRST_NAME = "^[A-Z][a-z]{2}[a-z]*";
 		final String LAST_NAME = "^[A-Z][a-z]{2}[a-z]*";
 
+		final String username = "^[A-Za-z][A-Za-z0-9+-]{2}[A-Za-z0-9+-]*([.][A-Za-z0-9]{2}[A-Za-z0-9]*)?";
+		final String domain = "\\@[a-z]{3}[a-z]*\\.[a-z]{2}[a-z]*([.][a-z]{2}[a-z]*)?";
+
+		final String EMAIL = username + domain;
+
 		String firstName = "Jayesh";
 		String lastName = "Kumar";
+		String email = "jayeshkumar8660@gmail.com";
 
 		if (Pattern.matches(FIRST_NAME, firstName)) { // USECASE1
 			System.out.println("Valid first name");
@@ -23,6 +29,12 @@ public class UserRegistration {
 			System.out.println("Valid last name");
 		} else {
 			System.out.println("Invalid last name");
+		}
+
+		if (Pattern.matches(EMAIL, email)) { // USECASE3
+			System.out.println("Valid email");
+		} else {
+			System.out.println("Invalid email");
 		}
 
 	}
